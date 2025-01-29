bc9c77f79a7f49f20a8843056087988a7078ee1a
tianyaxiang <tianyaxiang@qq.com>
2025-01-29 20:38:19 +0800

    clear code

--- src/components/templates/WechatTemplate.tsx
+++
@@ -1,9 +0,0 @@
-import { TemplateProps } from '@/types/template'
-
-export function WechatTemplate({ content, className }: TemplateProps) {
-  return (
-    <div className={`wechat-template ${className || ''}`}>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  )
-} 
--- src/components/templates/XiaohongshuTemplate.tsx
+++
@@ -1,9 +0,0 @@
-import { TemplateProps } from '@/types/template'
-
-export function XiaohongshuTemplate({ content, className }: TemplateProps) {
-  return (
-    <div className={`xiaohongshu-template ${className || ''}`}>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  )
-} 
