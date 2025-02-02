454441205d716ea5bc4047c1378fd469a85fd5b6
tianyaxiang <tianyaxiang@qq.com>
2025-02-02 19:50:59 +0800

    优化 loading

--- src/components/editor/components/EditorPreview.tsx
+++ src/components/editor/components/EditorPreview.tsx
@@ -173,7 +173,7 @@ export function EditorPreview({
       key={stableKeyRef.current}
     >
       <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b flex items-center justify-between z-10 sticky top-0 left-0 right-0">
-        <div className="flex items-center gap-0.5 px-2 py-1">
+        <div className="flex items-center gap-0.5 px-2 py-2">
           <span className="text-sm text-muted-foreground">预览效果</span>
         </div>
         <div className="flex items-center gap-4 px-4 py-1">
